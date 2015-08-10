80fe5079de531190aad4d5d98c8a76b155c748f8
Dmitry Filina <dmitry.filina@gmail.com>
2015-08-10 13:25:02 -0500

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,14 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+
+      # Send the email
+      ContactMailer.contact_email( name, email, body ).deliver
+
+      # Display message
       flash[:success] = 'Message sent.'
       redirect_to new_contact_path
     else
